50338faea29da9d963aa23b37416ab3e52471909
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-06-04 13:37:34 +0500

    All Tasks Completed

--- index.js
+++ index.js
@@ -1,231 +1,283 @@
-const prompt = require("prompt-sync")({ sigint: true });
-const convert = require("xml-js");
-const nlp = require("compromise");
-const qs = require("qs");
-const axios = require('axios');
+// REQUIREMENTS
 
-// Part of Speech Tags Mapping
-const partOfSpeechMap = {
-    "NN": "Noun",
-    "NNS": "Noun (plural)",
-    "NNP": "Proper Noun",
-    "NNPS": "Proper Noun  ",
-    "PRP": "Pronoun",
-    "VB": "Verb",
-    "VBD": "Verb  ",
-    "VBG": "Verb  ",
-    "VBN": "Verb  ",
-    "VBP": "Verb  ",
-    "VBZ": "Verb  ",
-    "RB": "Adverb",
-    "RBR": "Adverb  ",
-    "RBS": "Adverb  ",
-    "JJ": "Adjective",
-    "JJR": "Adjective  ",
-    "JJS": "Adjective  ",
-    "IN": "Preposition",
-    "DT": "Determiner",
-    "CC": " Conjunction",
-    "CD": "Cardinal Number",
-    "FW": "Foreign Word",
-    "LS": "List Item Marker",
-    "MD": "Modal",
-    "PDT": "Predeterminer",
-    "POS": "Possessive Ending",
-    "RP": "Particle",
-    "SYM": "Symbol",
-    "TO": "to",
-    "UH": "Interjection",
-    "WDT": "Wh-determiner",
-    "WP": "Wh-pronoun",
-    "WP$": "Possessive Wh-pronoun",
-    "WRB": "Wh-adverb"
-     
-};
+// 5 functions 1 curry function 1 calback 1 higher order 1 IIFE function 
+// 1 function is used to convert xml data to json
+//  2 filtering out of repeating words paragraph
+//  3 takes sentence and took  out parts of speech 
+// 4 fiboonacci series 
+//  5 pooling of same data conflict 
 
-// Function to generate the Fibonacci series up to a certain number
-function generateFibonacciSeriesBetween(start, end, callback) {
-    let firstNumber = 0,
-        secondNumber = 1,
-        nextTerm;
+import xml2js from "xml2js"
+import readline from "readline"
 
-    const series = [];
-
-    nextTerm = firstNumber + secondNumber;
-
-    // Generate Fibonacci series up to the upper limit
-    while (nextTerm <= end) {
-        if (nextTerm >= start) {
-            series.push(nextTerm);
-        }
-        firstNumber = secondNumber;
-        secondNumber = nextTerm;
-        nextTerm = firstNumber + secondNumber;
-    }
-
-    callback(series);
+async function prompt(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) => rl.question(question, (answer) => {
+    rl.close();
+    resolve(answer);
+  }));
 }
 
-// Callback function to display the series
-function displaySeries(series) {
-    if (series.length === 0) {
-        console.log("No Fibonacci numbers in the given range.");
+
+
+// // Repeating words
+// // using simple func
+function filterRepeat(para) {
+  const wordsArr = para.split(/\s+/);
+  const wordCounts = {};
+
+  for (const word of wordsArr) {
+    const lowerCaseWord = word.toLowerCase();
+    if (wordCounts[lowerCaseWord]) {
+      wordCounts[lowerCaseWord]++;
     } else {
-        console.log("Fibonacci Series in the given range:");
-        series.forEach((term) => console.log(term));
+      wordCounts[lowerCaseWord] = 1;
     }
+  }
+
+  const result = Object.keys(wordCounts).map(word => ({
+    word: word,
+    count: wordCounts[word]
+  }))
+
+  return result;
+}
+
+const paraContainingRepeatedWords = async () => {
+  try {
+    const para = await prompt("Enter paragraph: ");
+    const result = filterRepeat(para);
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
-// Curried function for converting XML to JSON
-const convertXmlToJson = (xmlData) => (options) => {
-    return convert.xml2json(xmlData, options);
+// Function to display the result by calling the iife function to filter out POS
+
+const partsOfSpeechMap = {
+  n: 'noun',
+  v: 'verb',
+  adj: 'adjective',
+  adv: 'adverb',
+  prep: 'preposition',
+  conj: 'conjunction',
+  interj: 'interjection',
+  pron: 'pronoun',
+  art: 'article'
 };
 
-const options = {
-    compact: true,
-    spaces: 2,
-};
+const filterPOS = async (sentence) => {
+  try {
+    const words = sentence.split(' ');
+    const results = [];
 
-// Higher-order function to create a part of speech finder
-const createPartOfSpeechFinder = (axiosInstance) => {
-    return async (text) => {
-        const apiKey = '7600cb7dd7716d78150be2aa102a75154f41b251203004ad966d6bb6'; // Replace with your actual API key
-        const url = 'https://api.textrazor.com/';
+    for (const word of words) {
+      const url = `https://api.datamuse.com/words?sp=${word}&md=p&max=1`;
+      const response = await fetch(url);
+      const data = await response.json();
 
-        try {
-            const response = await axiosInstance.post(url, qs.stringify({
-                extractors: 'words',
-                text
-            }), {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'x-textrazor-key': apiKey
-                }
+      if (data.length > 0 && data[0].tags && partsOfSpeechMap[data[0].tags[0]]) {
+        const result = {
+          word: word,
+          POS: partsOfSpeechMap[data[0].tags[0]]
+        };
+        results.push(result);
+      } else {
+        console.log(`No results found for "${word}"`);
+      }
+    }
+
+    console.log(results);
+    // return results;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// fibonacci series using curry functions
+const fibonacciSeriesInRange = (n) => {
+  if (n <= 1) {
+    return n;
+  }
+
+  let a = 0, b = 1, c;
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
+}
+
+async function FibbonacciUsingCurry() {
+  try {
+    const start = parseInt(await prompt("Enter start: "));
+    const end = parseInt(await prompt("Enter end: "));
+
+    for (let i = start; i <= end; i++) {
+      const num = fibonacciSeriesInRange(i)
+      if (num >= start && num <= end) {
+        console.log(num);
+      }
+    }
+
+  } catch (err) {
+    console.error('Error reading input:', err);
+  } finally {
+    process.exit()
+  }
+}
+
+
+// pool of user
+const data = {
+  num_of_persons: 0,
+  data: []
+}
+
+
+// xml to json
+function xmlToJson(xmlString) {
+  const parser = new xml2js.Parser();
+
+  return new Promise((resolve, reject) => {
+    parser.parseString(xmlString, function (err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+async function xmlString() {
+  try {
+    const xmlString = `<bookstore>
+    <book>
+      <title>Harry Potter</title>
+      <author>J.K. Rowling</author>
+    </book>
+    <book>
+      <title>Lord of the Rings</title>
+      <author>J.R.R. Tolkien</author>
+    </book>
+    <book>
+      <title>Chronicles of Narnia</title>
+      <author>C.S. Lewis</author>
+    </book>
+  </bookstore>`
+
+    const jsonString = await xmlToJson(xmlString);
+    console.log(`\n\t\t\XML : ${xmlString}\n\t\t ===> \t\t\nJSON : ${JSON.stringify(jsonString, null)}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+// calling on user choice 
+const calling = async () => {
+  try {
+    console.log("\x1b[36m"); // Cyan color
+
+    console.log("\n\t\t\x1b[1mWelcome To First TASK\x1b[0m\n");
+    console.log("\t\x1b[33mFiltering out of repeating words paragraph (press 1)\x1b[0m\n");
+    console.log("\t\x1b[35mFunction takes sentence and took out parts of speech (press 2)\x1b[0m\n");
+    console.log("\t\x1b[32mFibonacci series in range (press 3)\x1b[0m\n");
+    console.log("\t\x1b[31mpooling of same data conflict (press 4)\x1b[0m\n");
+    console.log("\t\x1b[34mXML To JSON (press 5)\x1b[0m\n");
+    const userInp = await prompt("\tEnter your choice: ");
+
+    switch (userInp) {
+      case "1":
+        paraContainingRepeatedWords()
+        break;
+      case "2":
+        (async function () {
+          const userInp = await prompt("Enter a sentence : ")
+          filterPOS(userInp)
+        })()
+        break
+      case "3":
+        FibbonacciUsingCurry()
+        break
+      case "4":
+        (async function () {
+          try {
+            const data = { num_of_persons: 0, data: [] };
+
+            const num_of_persons = parseInt(await prompt("Enter number of persons: "));
+            data.num_of_persons = num_of_persons;
+            for (let i = 0; i < num_of_persons; i++) {
+              const obj = {};
+              obj.name = await prompt(`Enter your name person ${i + 1} : `);
+              obj.age = parseInt(await prompt(`Enter your age person ${i + 1} : `));
+              obj.number = parseInt(await prompt(`Enter your number person ${i + 1} : `));
+              data.data.push(obj);
+            }
+            const realData = data.data
+            const nameSet = new Set();
+            const ageSet = new Set();
+            const numberSet = new Set();
+            const processedObjects = new Set();
+            const duplicatesOfName = [];
+            const duplicatesOfAge = [];
+            const duplicatesOfNumber = [];
+
+            realData.forEach((obj) => {
+              const objString = JSON.stringify(obj);
+
+              if (nameSet.has(obj.name) && !processedObjects.has(`${objString}-name`)) {
+                duplicatesOfName.push(obj);
+                processedObjects.add(`${objString}-name`)
+              } else {
+                nameSet.add(obj.name);
+              }
+
+              if (ageSet.has(obj.age) && !processedObjects.has(`${objString}-age`)) {
+                duplicatesOfAge.push(obj);
+                processedObjects.add(`${objString}-age`);
+              } else {
+                ageSet.add(obj.age);
+              }
+
+              if (numberSet.has(obj.number) && !processedObjects.has(`${objString}-number`)) {
+                duplicatesOfNumber.push(obj);
+                processedObjects.add(`${objString}-number`);
+              } else {
+                numberSet.add(obj.number);
+              }
             });
 
-            const data = response.data;
-            console.log('Full response:', JSON.stringify(data, null, 2)); // Pretty print the full response for better readability
+            console.log("Real data:", data);
+            console.log("Conflicting data:");
+            console.log("Duplicate Names:", duplicatesOfName);
+            console.log("Duplicate Ages:", duplicatesOfAge);
+            console.log("Duplicate Numbers:", duplicatesOfNumber);
+          } catch (err) {
+            console.log(err);
+          }
+        })()
+        break
+      case "5":
+        xmlString()
+        break
+      default:
+        console.log("Invalid input")
+        break
 
-            // Explore the response structure to find the correct path to 'words'
-            if (data.response && data.response.sentences) {
-                const partOfSpeechTags = data.response.sentences.flatMap(sentence =>
-                    sentence.words.map(word => ({
-                        word: word.token,
-                        tag: partOfSpeechMap[word.partOfSpeech] || word.partOfSpeech
-                    }))
-                );
-                console.log('Part of Speech Tags:', partOfSpeechTags);
-            } else {
-                console.log('No Part of Speech tags found.');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
-};
-
-// IIFE function to handle user inputs and selections
-(  async function() {
-    const users = [];
-    const polls = [];
-
-    function addUser(firstName, lastName, phoneNumber, emailAddress) {
-        const newUser = { firstName, lastName, phoneNumber, emailAddress };
-        let foundDuplicate = false;
-        const relatedUsers = [];
-
-        users.forEach(user => {
-            if (user.phoneNumber === phoneNumber || user.emailAddress === emailAddress || user.lastName === lastName || user.firstName === firstName) {
-                foundDuplicate = true;
-                relatedUsers.push(user);
-            }
-        });
-
-        if (!foundDuplicate) {
-            // If no duplicate is found, add the new user
-            users.push(newUser);
-            console.log(`User added: ${JSON.stringify(newUser)}`);
-        } else {
-            // If a duplicate is found, handle duplicates
-            relatedUsers.forEach(user => polls.push({ user }));
-            polls.push({ user: newUser });
-            console.log(`Duplicate user detected. Created entries for duplicates.`);
-
-            // Ensure the new user is included in the main users list
-            if (!users.includes(newUser)) {
-                users.push(newUser);
-            }
-        }
     }
 
-    function promptUserForDetails() {
-        const firstName = prompt('Enter first name: ');
-        const lastName = prompt('Enter last name: ');
-        const phoneNumber = prompt('Enter phone number: ');
-        const emailAddress = prompt('Enter email address: ');
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-        addUser(firstName, lastName, phoneNumber, emailAddress);
-    }
 
-    const key = prompt(
-        "Which Function you want to execute: \n(1) Callback (Fibonacci) (2) Curry (XML to JSON)(3) IIFE (word counter) (4) HOF (part of speech) (5) HOF (Add User)\n"
-    );
-
-    switch (key) {
-        case "1":
-          
-
-            const start = parseInt(prompt("Enter the start number of the range: "));
-            const end = parseInt(prompt("Enter the end number of the range: "));
-            
-            if (isNaN(start) || isNaN(end)) {
-                console.log("Please enter valid numbers.");
-            } else {
-                generateFibonacciSeriesBetween(start, end, displaySeries);
-            }
-            break;
-        case "2":
-            const xmlData = prompt('Please enter XML data: ');
-
-            // Convert XML to JSON
-            const jsonResult = convertXmlToJson(xmlData)(options);
-
-            console.log('Converted JSON:');
-            console.log(jsonResult);
-            break;
-        case "3":
-            const wordCount = (() => {
-                return function(input) {
-                    const words = input.split(" ");
-                    const result = {};
-                    words.forEach((word) => {
-                        result[word] = result[word] + 1 || 1;
-                    });
-                    return result;
-                };
-            })();
-
-            const inputParagraph = prompt("Enter the paragraph: ");
-            const counts = wordCount(inputParagraph);
-            console.log(counts);
-            break;
-        case "4":
-            const findPartOfSpeech = createPartOfSpeechFinder(axios);
-            const sentence = prompt("Enter the sentence: ");
-            await findPartOfSpeech(sentence);
-            break;
-        case "5":
-            let moreUsers = true;
-            while (moreUsers) {
-                promptUserForDetails();
-                const more = prompt('Do you want to add another user? (yes/no): ').toLowerCase();
-                moreUsers = (more === 'yes');
-            }
-            console.log('All users:', users);
-            console.log('Entries for duplicate users:', polls);
-            break;
-        default:
-            console.log("Invalid choice.");
-    }
-})();
+calling()
